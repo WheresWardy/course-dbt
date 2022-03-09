627dcfefb80bb7f147f3479dec7cc91bdac80fc7
Matthew Ward <mattward41@gmail.com>
2022-03-09 18:10:29 +0000

    Add basic staging environment

---
+++ greenery/models/staging/stg_users.sql
@@ -0,0 +1,12 @@
+
+SELECT
+    user_id,
+    first_name,
+    last_name,
+    email,
+    phone_number,
+    created_at,
+    updated_at,
+    address_id
+
+FROM {{ source('greenery_db', 'users') }}
