51bf6d2e1a49d9a6171e06b6d1d558303309c3d7
Matthew Ward <matt.ward@lyst.com>
2022-03-12 12:48:21 +0000

    Add descriptions to model schema

--- greenery/models/staging/stg_events.sql
+++ greenery/models/staging/stg_events.sql
@@ -10,9 +10,9 @@ renamed_recast AS (
         event_id,
         session_id,
         user_id,
-        event_type,
         page_url,
         created_at,
+        event_type,
         order_id,
         product_id
     FROM source
